ba05fa7c26f73cb55ad27c38e06e628a15face22
gowthamkoffcl <gowthamkoffcl@gmail.com>
2023-12-28 13:00:15 +0530

    user module already exists middleware - completed

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const { connection } = require("./src/configs/database");
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+const swagger = require('swagger-ui-express')
 require('dotenv').config()
 
 
-app.use(express.json());
-app.use(cors());
 
+
+
+
+// Middlewares
+app.use(express.json());
+app.use(cors())
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
@@ -15,7 +22,12 @@ app.use(function (req, res, next) {
     next();
 })
 
+
+//  Database Connections
 connection()
+
+
+app.use('/api-docs', swagger.serve, swagger.setup(swaggerDocument))
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on http://localhost:${process.env.PORT}`);
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,53 +1,86 @@
 const { sequelize } = require("../configs/database");
+const { Op, where } = require("sequelize");
+const { DataTypes } = require('sequelize');
+const { Failure } = require("../common/response.model");
+const { checkNull } = require("../common/common");
 
-const { DataTypes } = require('sequelize')
+const USER = sequelize.define('users', {
+    userId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    name: {
+        type: DataTypes.STRING(64),
+        allowNull: false,
+    },
+    mobileNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: 'user'
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    status: {
+        type: DataTypes.INTEGER,
+        defaultValue: 1
+    },
+    profileImage: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    }
+})
 
 class UserModel {
+    user
+    constructor() {
+        this.user = USER
+    }
 
-    constructor() { }
 
-    user = sequelize.define('users', {
-        userId: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: false,
-        },
-        name: {
-            type: DataTypes.STRING(64),
-            allowNull: false,
-        },
-        mobileNumber: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true
-        },
 
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true
-        },
-        type: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            defaultValue: 'user'
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        status: {
-            type: DataTypes.INTEGER,
-            defaultValue: 1
-        },
-        profileImage: {
-            type: DataTypes.STRING,
-            allowNull: true,
+
+    async checkDuplicate(req, res, next) {
+
+
+        console.log(req.body)
+        const { email, mobileNumber } = req.body;
+
+        const existingMobile = await USER.findOne({ where: { mobileNumber } })
+
+        const existingEmail = await USER.findOne({ where: { email } })
+
+        if (!checkNull(existingMobile)) {
+
+            if (!checkNull(existingEmail)) {
+
+                next()
+
+            } else {
+                res.status(500).json(new Failure('Email already exists'))
+            }
+
+
+        } else {
+            res.status(500).json(new Failure('Mobile already exists'))
         }
-    })
+    }
 
 }
- 
+
 
 module.exports = UserModel
