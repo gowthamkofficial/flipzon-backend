d9eef4a1beddc62182657e2b008eadd1b2366402
gowthamkoffcl <gowthamkoffcl@gmail.com>
2023-12-28 10:20:34 +0530

    Added the readme option and cors functionality

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const { connection } = require("./src/configs/database");
 require('dotenv').config()
 
 
 app.use(express.json());
+app.use(cors());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
 connection()
 
 app.listen(process.env.PORT, () => {
